7d468a6adbe43ad161b4e9f0605bed93ef1f57dd
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2020-03-10 23:30:16 -0300

    creating base for POST createUser endpoint

---
+++ src/main/java/com/springws/app/service/UserService.java
@@ -0,0 +1,7 @@
+package com.springws.app.service;
+
+import com.springws.app.shared.dto.UserDto;
+
+public interface UserService {
+	UserDto createUser(UserDto user);
+}
---
+++ src/main/java/com/springws/app/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.springws.app.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.springws.app.service.UserService;
+import com.springws.app.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	public UserDto createUser(UserDto user) {
+		return null;
+	}
+}
